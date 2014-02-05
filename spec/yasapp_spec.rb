6ee6f041171a175d5ec5e8ef872aa9054276a65c
Harm Behrens <hb@netzfisch.de>
2014-02-05 23:05:25 +0100

    implement example PUT request
    * to reverse string

--- spec/yasapp_spec.rb
+++ spec/yasapp_spec.rb
@@ -1,18 +1,28 @@
 require 'spec_helper'
 
-describe "GET show '/'" do
-  it "allows home page access" do
-    get '/'
-    expect(last_response).to be_ok
+describe "YaSApp" do
+  describe "GET show at '/'" do
+    it "allows home page access" do
+      get '/'
+      expect(last_response).to be_ok
+    end
+
+    it "sends http status code '200'" do
+      get '/'
+      expect(last_response.status).to eq 200
+    end
+
+    it "shows welcome message" do
+      get '/'
+      expect(last_response.body).to match 'Welcome to the Sinatra course at RubyLearning. Yeah, that\'s awesome!'
+    end
   end
 
-  it "sends http status code '200'" do
-    get '/'
-    expect(last_response.status).to eq 200
-  end
-
-  it "shows welcome message" do
-    get '/'
-    expect(last_response.body).to match 'Welcome to the Sinatra course at RubyLearning. Yeah, that\'s awesome!'
+  describe "POST create at '/reverse'" do
+    # curl -X -d "reverse me" localhost:5000/reverse/Hello
+    it "reverses posted value" do
+      post '/reverse/Hello' #, params = {:str => 'Hello'}
+      expect(last_response.body).to eq 'olleH'
+    end
   end
 end
--- yasapp.rb
+++ yasapp.rb
@@ -3,10 +3,18 @@ require 'sinatra/reloader' if development?
 
 set :display_string, 'Welcome to the Sinatra course at RubyLearning.'
 
+def reverse string
+  string.each_char.to_a.reverse
+end
+
 before do
   @msg = 'Yeah, that\'s awesome!'
 end
 
 get '/' do
   settings.display_string + " " + @msg
+end
+
+post '/reverse/:str' do
+  reverse params[:str]
 end
