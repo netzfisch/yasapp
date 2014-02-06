6765161cdba17c03a51ce333e32ef3c82479f9d2
Harm Behrens <hb@netzfisch.de>
2014-02-06 18:36:33 +0100

    restructure application
    * put the different 'functions' in one seperate view 'index.haml'
    * evaluate current date in before filter and hand it over to view
      via instance variable
    * reverse service
      * put reverse method in helper block
      * differentiate reversing string between
        *  html form and
        * http request
      * redirect to view template 'show.haml'

--- spec/yasapp_spec.rb
+++ spec/yasapp_spec.rb
@@ -7,29 +7,32 @@ describe "Yet annother Sinatra Application" do
       expect(last_response).to be_ok
     end
 
-    it "sends http status code '200'" do
+    it "sends status code '200'" do
       get '/'
       expect(last_response.status).to eq 200
     end
 
     it "shows welcome message" do
       get '/'
-      expect(last_response.body).to match 'Welcome to the Sinatra course at RubyLearning. Yeah, that\'s awesome!'
+      expect(last_response.body).to match 'Welcome to "Yet'
     end
-  end
 
-  describe "GET show '/haml-view'" do
     it "shows current time" do
       Time.stub(:now).and_return('2014-02-06 14:08:31')
 
-      get '/haml-view'
+      get '/'
       expect(last_response.body).to match '14:08'
     end
   end
 
   describe "POST create '/reverse'" do
-    it "reverses posted value" do
-      post '/reverse/Hello' #, params = {:str => 'Hello'}
+    it "reverses value from html form" do
+      post '/reverse', params = {:str => 'Hello'}
+      expect(last_response.body).to match 'olleH'
+    end
+
+    it "reverses value from http request" do
+      post '/reverse/Hello'
       expect(last_response.body).to eq 'olleH'
     end
   end
--- yasapp.rb
+++ yasapp.rb
@@ -2,26 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'haml'
 
-set :display_string, 'Welcome to the Sinatra course at RubyLearning.'
-
-def reverse string
-  string.each_char.to_a.reverse
+helpers do
+  def backwards string
+    string.reverse
+  end
 end
 
 before do
-  @msg = 'Yeah, that\'s awesome!'
+  @current_date = Time.now.to_s
 end
 
 get '/' do
-  settings.display_string + " " + @msg
-end
-
-get '/haml-view' do
   haml :index
 end
 
-# will not work in browser, need to be triggered via
-# curl -X -d "reverse me" localhost:5000/reverse/Hello
+post '/reverse' do
+  @reverse_value = backwards params[:str]
+  haml :show
+end
+
+# will not work in browser, needs to be triggered via
+# $ curl -X -d "reverse me" localhost:5000/reverse/Hello
 post '/reverse/:str' do
-  reverse params[:str]
+  backwards params[:str]
 end
