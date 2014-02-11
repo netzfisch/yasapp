51f39a0b86f10b4f1ba7a2e4f4ed25c50d24199d
Harm Behrens <hb@netzfisch.de>
2014-02-11 17:34:14 +0100

    satisfy heroku demands

--- models/participant.rb
+++ models/participant.rb
@@ -1,9 +1,26 @@
 require 'sinatra/activerecord'
+require 'active_record'
 
-configure do
+configure :development do
   # connect to the database
   dbconfig = YAML.load(File.read('config/database.yml'))
   ActiveRecord::Base.establish_connection dbconfig['development']
+
+  begin
+    ActiveRecord::Schema.define do
+      create_table :participants do |t|
+        t.string :name, :null => false, :limit => 100
+        t.string :email, :null => false, :limit => 50
+      end
+    end
+  rescue ActiveRecord::StatementInvalid
+    # do nothing - gobble up the error
+  end
+end
+
+configure :production do
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
   begin
     ActiveRecord::Schema.define do
       create_table :participants do |t|
